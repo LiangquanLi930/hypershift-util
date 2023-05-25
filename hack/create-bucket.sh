ec0fa92e88cab8fc9172239f5c933d9fb3cab86c
Liangquan Li <liangli@redhat.com>
2023-05-25 13:13:32 +0800

    update create aws bucket

--- hack/create-bucket.sh
+++ hack/create-bucket.sh
@@ -13,13 +13,29 @@ if [ $? -eq 0 ] ; then
     echo "this bucket already exists"
 else
     if [ "$REGION" == "us-east-1" ]; then
-        aws s3api create-bucket --acl public-read --bucket "$BUCKET_NAME" \
+        aws s3api create-bucket --bucket "$BUCKET_NAME" \
             --region us-east-1
+        aws s3api delete-public-access-block --bucket "$BUCKET_NAME"
         echo -e "$BUCKET_NAME\t$REGION" >> "config/aws_bucket"
     else
-        aws s3api create-bucket --acl public-read --bucket "$BUCKET_NAME" \
+        aws s3api create-bucket --bucket "$BUCKET_NAME" \
             --create-bucket-configuration LocationConstraint="$REGION" \
             --region "$REGION"
+        aws s3api delete-public-access-block --bucket "$BUCKET_NAME"
         echo -e "$BUCKET_NAME\t$REGION" >> "config/aws_bucket"
     fi
+    aws s3api delete-public-access-block --bucket "$BUCKET_NAME"
+    export BUCKET_NAME=$BUCKET_NAME
+    echo '{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${BUCKET_NAME}/*"
+            }
+        ]
+    }' | envsubst > config/policy.json
+    aws s3api put-bucket-policy --bucket "$BUCKET_NAME" --policy file://config/policy.json
 fi
