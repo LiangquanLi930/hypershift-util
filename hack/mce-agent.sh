1958c8dde8d17557b0ed6bad70cc7388ae114191
Liangquan Li <liangli@redhat.com>
2023-10-10 01:17:51 +0800

    add mce-agent.sh

---
+++ hack/mce-agent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -exuo pipefail
+
+downURL=$(oc get ConsoleCLIDownload hcp-cli-download -o json | jq -r '.spec.links[] | select(.text | test("Linux for x86_64")).href') && curl -k --output /tmp/hcp.tar.gz ${downURL}
+pushd /tmp && tar -xvf /tmp/hcp.tar.gz
+chmod +x /tmp/hcp
+popd
+
+CLUSTER_NAME="test01"
+echo "$(date) Creating HyperShift cluster ${CLUSTER_NAME}"
+oc create ns "local-cluster-${CLUSTER_NAME}"
+BASEDOMAIN=$(oc get dns/cluster -ojsonpath="{.spec.baseDomain}")
+PLAYLOADIMAGE=$(oc get clusterversion version -ojsonpath='{.status.desired.image}')
+echo "extract secret/pull-secret"
+oc extract secret/pull-secret -n openshift-config --to=/tmp --confirm
+
+/tmp/hcp create cluster agent \
+  --name=${CLUSTER_NAME} \
+  --pull-secret=/tmp/.dockerconfigjson \
+  --namespace local-cluster \
+  --agent-namespace="local-cluster-${CLUSTER_NAME}" \
+  --base-domain=${BASEDOMAIN} \
+  --release-image "$PLAYLOADIMAGE"
